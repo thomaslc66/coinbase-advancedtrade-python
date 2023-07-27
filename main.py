ff8e37b9272f8a9d7865342a9ee29f6b2872a8cb
thomaslc66 <thomaslc66@users.noreply.github.com>
2023-07-27 15:59:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from telegram_bot import run_telegram_bot
 
 # Set your API key and secret
-API_KEY = "62qaVqy81fPlXFu5"
-API_SECRET = "RkmDjjR2qH2OgJyM18wGow88yUSvH45W"
-TELEGRAM = "6422868383:AAGJJRhbbMbyC8y9fS8lk1cWi-CvJF8UYc8"
+API_KEY = ""
+API_SECRET = ""
+TELEGRAM = ""
 
 if __name__ == '__main__':
     run_telegram_bot(API_KEY, API_SECRET, TELEGRAM)
